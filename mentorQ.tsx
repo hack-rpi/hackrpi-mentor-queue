c61bb604be11f052591c9360b52f829d95a8b512
CharlieC133 <69218910+CharlieC133@users.noreply.github.com>
2024-04-13 16:05:50 -0400

    Update mentorQ.tsx
    implementation of in queue checker that reutrns a bool. Also made some error checking parts

--- mentorQ.tsx
+++ mentorQ.tsx
@@ -27,11 +27,33 @@ const getRequestsData = async () => {
         const requestRef = collection(db, 'requests');
         const querySnapshot = await getDocs(requestRef);
         
+        // checks if the requests database exists or not if it deos not exist then it weill log it
         if (querySnapshot.exists()) {
+            //commmented beflow is a error checking function that logs whats being returned
+            //console.log(querySnapshot.data())
             return querySnapshot.data();
-          } else {
-            // docSnap.data() will be undefined in this case
-            console.log("No such document!");
-          }
-    }
-}
+        } 
+        else {console.log("No such document!");}
+
+    }catch(error){console.error('Error retrieving data:', error);}
+};
+
+
+// This function checks whether a student is in the queue based on the document key.
+// It returns whether the student has been helped or not.
+const inQueue = async (docKey) => {
+    try {
+        // Grabs the document
+        const docRef = db.collection('requests').doc(docKey);
+        // Gets the data from the doc
+        const doc = await docRef.get();
+
+        // Ensures that the document is retrieved properly
+        if (!doc.exists) { console.log('No such document!');}
+        else {
+        // below commented is a log function that could help error check
+        // console.log('Document data:', doc.data());
+            return doc.data().helped;
+        }
+    } catch (error) {console.error('Error retrieving document:', error);}
+};
